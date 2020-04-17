ee1557a028b79af7694e75321efb3668e932ded0
Michael Weiner <vikings412@gmail.com>
2020-04-17 08:52:09 -0500

    fixed indentation for all files

--- maw-resources-post-type.php
+++ maw-resources-post-type.php
@@ -12,52 +12,52 @@ if ( ! defined( 'ABSPATH' ) ) {
 *
 */
 function maw_resources_custom_post_type() {
-$supports = array(
-    'title', // Post title
-    //'editor', // Post content
-    'author', // Post author
-    //'thumbnail', // Featured images
-    //'excerpt', // Post excerpt
-    'custom-fields', // Custom fields
-    //'comments', // Post comments
-    //'revisions', // Post revisions
-    //'post-formats', // Post formats
-);
+    $supports = array(
+        'title', // Post title
+        //'editor', // Post content
+        'author', // Post author
+        //'thumbnail', // Featured images
+        //'excerpt', // Post excerpt
+        'custom-fields', // Custom fields
+        //'comments', // Post comments
+        //'revisions', // Post revisions
+        //'post-formats', // Post formats
+    );
 
-$labels = array(
-'name' => _x('Resources', 'plural'),
-'singular_name' => _x('Resource', 'singular'),
-'menu_name' => _x('Resources', 'admin menu'),
-'name_admin_bar' => _x('Resources', 'admin bar'),
-'add_new' => _x('Add New Resource', 'add new'),
-'add_new_item' => __('Add New Resource'),
-'new_item' => __('New Resource'),
-'edit_item' => __('Edit Resource'),
-'view_item' => __('View Resource'),
-'all_items' => __('All Resources'),
-'search_items' => __('Search Resources'),
-'not_found' => __('No Resources Found.'),
-);
+    $labels = array(
+        'name' => _x('Resources', 'plural'),
+        'singular_name' => _x('Resource', 'singular'),
+        'menu_name' => _x('Resources', 'admin menu'),
+        'name_admin_bar' => _x('Resources', 'admin bar'),
+        'add_new' => _x('Add New Resource', 'add new'),
+        'add_new_item' => __('Add New Resource'),
+        'new_item' => __('New Resource'),
+        'edit_item' => __('Edit Resource'),
+        'view_item' => __('View Resource'),
+        'all_items' => __('All Resources'),
+        'search_items' => __('Search Resources'),
+        'not_found' => __('No Resources Found.'),
+    );
 
-$args = array(
-'labels' => $labels,
-'description' => 'A custom post type to display resources.',
-'public' => true,
-'hierarchical' => true,
-'exclude_from_search' => false,
-'publicly_queryable' => false, // Set this to be false to hide the single and archive page
-'show_ui' => true,
-'show_in_menu' => true,
-'supports' => $supports,
-'has_archive' => true,
-'rewrite' => array('slug' => 'maw-resources'),
-'query_var' => true,
-'taxonomies' => array( 'category', 'post_tag' ),
-);
-register_post_type('maw-resources', $args); // Register our post type with Wordpress
+    $args = array(
+        'labels' => $labels,
+        'description' => 'A custom post type to display resources.',
+        'public' => true,
+        'hierarchical' => true,
+        'exclude_from_search' => false,
+        'publicly_queryable' => false, // Set this to be false to hide the single and archive page
+        'show_ui' => true,
+        'show_in_menu' => true,
+        'supports' => $supports,
+        'has_archive' => true,
+        'rewrite' => array('slug' => 'maw-resources'),
+        'query_var' => true,
+        'taxonomies' => array( 'category', 'post_tag' ),
+    );
+    register_post_type('maw-resources', $args); // Register our post type with Wordpress
 }
 add_action('init', 'maw_resources_custom_post_type');
-/*
+/*  
 *
 * END: Custom Post Type 'maw-resources'
 *
@@ -95,10 +95,11 @@ add_filter('pre_get_posts', 'maw_sort_resources_post_by_title');
 *
 */
 function maw_resources_post_type_disable_comments( $post_id ) {
-$post_type = get_post_type( $post_id );
-if ( $post_type == 'maw-resources' ) {
-return false;
-}
+    $post_type = get_post_type( $post_id );
+    
+    if ( $post_type == 'maw-resources' ) {
+        return false;
+    }
 }
 add_filter( 'comments_open', 'my_prefix_comments_open', 10 , 1 );
 /*
--- maw-resources-shortcode.php
+++ maw-resources-shortcode.php
@@ -11,97 +11,97 @@ if ( ! defined( 'ABSPATH' ) ) {
 *
 */
 function maw_resources_posts_shortcode($atts) {
+    extract(shortcode_atts(array(
+        'cat'     => '',
+        'ignore_sticky_posts' => false,
+        'maw_show_resource_author' => true,
+        'maw_show_resource_description' => true,
+        'meta_key' => '',
+        'num'     => '5',
+        'order'   => 'ASC',
+        'orderby' => 'title',
+        'offset'  => '',
+        'page_id' => '',
+        'post_type' => 'maw-resources',
+        'tag' => '',
+    ), $atts));
 
-extract(shortcode_atts(array(
-'cat'     => '',
-'ignore_sticky_posts' => false,
-'maw_show_resource_author' => true,
-'maw_show_resource_description' => true,
-'meta_key' => '',
-'num'     => '5',
-'order'   => 'ASC',
-'orderby' => 'title',
-'offset'  => '',
-'page_id' => '',
-'post_type' => 'maw-resources',
-'tag' => '',
-), $atts));
-
-$args = array(
-'cat'            => $cat,
-'maw_show_resource_author' => $maw_show_resource_author,
-'maw_show_resource_description' => $maw_show_resource_description,
-'meta_key' => $meta_key,
-'posts_per_page' => $num,
-'offset'         =>  $offset,
-'order'          => $order,
-'orderby'        => $orderby,
-'page_id'        => $page_id,
-'post_type'      => $post_type,
-'num'     => $num,
-'tag'            => $tag,
-);
-
-/*
-*  Extract values of the custom shortcode modifiers
-*/
-$maw_display_show_author = esc_attr($args['maw_show_resource_author']);
-$maw_display_show_description = esc_attr($args['maw_show_resource_description']);
-
-$maw_output = ''; // Establish variable to print HTML for display
-
-$maw_query = new WP_Query( $args );
-if( $maw_query->have_posts() ){
-
-/*
-* Begin HTML Output
-*/
-$maw_output .= '<div class="maw_container"><div class="maw_header"><div class="maw_title">Name</div><div class="maw_link">Link</div><br class="maw_clear"></div>';
-
-    while( $maw_query->have_posts() ){
-    $maw_query->the_post();
+    $args = array(
+        'cat'            => $cat,
+        'maw_show_resource_author' => $maw_show_resource_author,
+        'maw_show_resource_description' => $maw_show_resource_description,
+        'meta_key' => $meta_key,
+        'posts_per_page' => $num,
+        'offset'         =>  $offset,
+        'order'          => $order,
+        'orderby'        => $orderby,
+        'page_id'        => $page_id,
+        'post_type'      => $post_type,
+        'num'     => $num,
+        'tag'            => $tag,
+    );
 
     /*
-    * Get the various resource post attributes
+    *  Extract values of the custom shortcode modifiers
     */
-    $maw_resource_id = get_the_ID(); // Get the id of the resource
-    $maw_resource_title = get_the_title(); // Get and store the title of the resource
-    $maw_resource_author = get_post_meta($maw_resource_id, 'maw-resource-publisher', true); // Store the resource's publishing entity entered by the user
-    $maw_resource_date = get_post_meta($maw_resource_id, 'maw-publish-date', true); // Store the resource's publication date entered by the user
-    $maw_resource_url = get_post_meta($maw_resource_id, 'maw-resource-url', true); // Store the resource's URL
-    $maw_resource_description = get_post_meta($maw_resource_id, 'maw-resource-description', true); // Store the resource's description entered by the user
+    $maw_display_show_author = esc_attr($args['maw_show_resource_author']);
+    $maw_display_show_description = esc_attr($args['maw_show_resource_description']);
+
+    $maw_output = ''; // Establish variable to print HTML for display
+
+    $maw_query = new WP_Query( $args );
+    if( $maw_query->have_posts() ){
 
     /*
-    * Finish HTML Output
+    * Begin HTML Output
     */
-    $maw_output .= '<div class="maw_item">'; // Add the <div> for the item
-            $maw_output .= '<div class="maw_title"><b>'. $maw_resource_title .'</b>'; // Add the title <div> and the title of the post
+    $maw_output .= '<div class="maw_container"><div class="maw_header"><div class="maw_title">Name</div><div class="maw_link">Link</div><br class="maw_clear"></div>';
 
-                    if ($maw_display_show_author === 'false' || $maw_resource_author == null) { // Check to see if the user has disallowed the author in the display OR that the the resource has an author and if so, hide it
-                    } else {
-                    $maw_output .= '<div class="maw_resource_author"><em>Published By: '. $maw_resource_author .'</em></div>';
-                    }
+        while( $maw_query->have_posts() ){
+        $maw_query->the_post();
 
-                    if ($maw_display_show_description === 'false' || $maw_resource_description == null) { // Check to see if the user has disallowed the description in the display OR that the resource does not have a short description, and if so hide it
-                    } else {
-                    $maw_output .= '<div class="maw_resource_description">'. $maw_resource_description .'<br class="maw_clear"></div>';
+        /*
+        * Get the various resource post attributes
+        */
+        $maw_resource_id = get_the_ID(); // Get the id of the resource
+        $maw_resource_title = get_the_title(); // Get and store the title of the resource
+        $maw_resource_author = get_post_meta($maw_resource_id, 'maw-resource-publisher', true); // Store the resource's publishing entity entered by the user
+        $maw_resource_date = get_post_meta($maw_resource_id, 'maw-publish-date', true); // Store the resource's publication date entered by the user
+        $maw_resource_url = get_post_meta($maw_resource_id, 'maw-resource-url', true); // Store the resource's URL
+        $maw_resource_description = get_post_meta($maw_resource_id, 'maw-resource-description', true); // Store the resource's description entered by the user
 
-                    }
+        /*
+        * Finish HTML Output
+        */
+        $maw_output .= '<div class="maw_item">'; // Add the <div> for the item
+                $maw_output .= '<div class="maw_title"><b>'. $maw_resource_title .'</b>'; // Add the title <div> and the title of the post
 
-                    if ($maw_resource_url != null) { // Check to see if the resource has a URL, and if so display it
-                    $maw_output .= '</div><div class="maw_link"><a href="'. get_post_meta($maw_resource_id, 'maw-resource-url', true) .'" title="View Resource" target="_blank">View Resource</a><br></div><br class="maw_clear"></div>';
-            } else {
-            $maw_output .= '</div><br class="maw_clear"></div>';
-    }
+                        if ($maw_display_show_author === 'false' || $maw_resource_author == null) { // Check to see if the user has disallowed the author in the display OR that the the resource has an author and if so, hide it
+                        } else {
+                        $maw_output .= '<div class="maw_resource_author"><em>Published By: '. $maw_resource_author .'</em></div>';
+                        }
+
+                        if ($maw_display_show_description === 'false' || $maw_resource_description == null) { // Check to see if the user has disallowed the description in the display OR that the resource does not have a short description, and if so hide it
+                        } else {
+                        $maw_output .= '<div class="maw_resource_description">'. $maw_resource_description .'<br class="maw_clear"></div>';
+
+                        }
+
+                        if ($maw_resource_url != null) { // Check to see if the resource has a URL, and if so display it
+                        $maw_output .= '</div><div class="maw_link"><a href="'. get_post_meta($maw_resource_id, 'maw-resource-url', true) .'" title="View Resource" target="_blank">View Resource</a><br></div><br class="maw_clear"></div>';
+                } else {
+                $maw_output .= '</div><br class="maw_clear"></div>';
+        }
     }
     $maw_output .= '</div></div></div>'; // Close maw_item, maw_header, and maw_container
-} else {
-$maw_output .= '<div><p class="maw_empty_message">We are sorry. There are no posts that fit your criteria to display.</p></div>'; // Return error message if no shortcode requirements find any matching posts
-}
 
-wp_reset_postdata(); // Clear the query data for future queries
+    } else {
+        $maw_output .= '<div><p class="maw_empty_message">We are sorry. There are no posts that fit your criteria to display.</p></div>'; // Return error message if no shortcode requirements find any matching posts
+    }
 
-return $maw_output; // Return the HTML to be displayed
+    wp_reset_postdata(); // Clear the query data for future queries
+
+    return $maw_output; // Return the HTML to be displayed
 }
 add_shortcode('maw_resources', 'maw_resources_posts_shortcode');
 /*
