c6f97cb4a37a8882c6defd89acb56c568b5a2809
Michael Weiner <vikings412@gmail.com>
2020-04-18 11:35:47 -0500

    broke single-post-template css into resources.css file, implemented post title, and fixed view resource link

--- includes/template-overrides/single-maw-resources.php
+++ includes/template-overrides/single-maw-resources.php
@@ -9,32 +9,30 @@ $maw_publisher_url = get_post_meta( get_the_ID(), 'maw-resource-url', true );
 
     <div class="container">
         <div id="content-area" class="clearfix">
-            <div id="maw-post-content" style="padding: 4em;">
-                <div id="maw-post-title-meta" style="margin-bottom: 1em;">
-                    <h2>Post Title</h2>
+            <div id="maw-post-content">
+                <div id="maw-post-title-meta">
+                    <h2><?php echo get_the_title( $post_id ); ?></h2>
 
                     <p id="maw-post-resource-date-field">
                         <?php echo date('F j, Y', $maw_publication_publish_date_text ); ?>
                     </p>
                 </div>
 
-                <div class="maw_clear"></div>
-
-                <div id="maw-post-content-description" style="margin-bottom: 1em;"> 
+                <div id="maw-post-content-description"> 
                     <p>
                         <?php echo esc_html( $maw_resource_description_text ); ?>
                     </p>
                 </div>
 
-                <div id="maw-post-link" style="margin-bottom: 1em;">
-                    <a href=<?php echo esc_html( $maw_publisher_url); ?> target="_blank"><p id="maw-post-link-text">View Resource ></p></a>
+                <div id="maw-post-link">
+                    <p id="maw-post-link-text"><a href=<?php echo esc_html( $maw_publisher_url); ?> target="_blank">View Resource ></p></a>
                 </div>
 
 
                 <?php if($maw_publisher_name_text != "") { ?>
 
-                    <div id="maw-publisher-thank-you" style="width=100%; background:#f2f2f2; padding:10px;">
-                        <p style="text-align: center;">A special thank you to <?php echo esc_html( $maw_publisher_name_text ); ?> as the original publisher of this resource.</p>
+                    <div id="maw-publisher-thank-you">
+                        <p id="maw-publisher-thank-you-text">A special thank you to <?php echo esc_html( $maw_publisher_name_text ); ?> as the original publisher of this resource.</p>
                     </div>
                 
                 <?php } ?>
