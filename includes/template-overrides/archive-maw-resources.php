3be12896c54a2110e23a13680a4128ec522a7303
Michael Weiner <vikings412@gmail.com>
2021-01-11 14:10:15 -0600

    clean html & php code

--- includes/template-overrides/archive-maw-resources.php
+++ includes/template-overrides/archive-maw-resources.php
@@ -8,59 +8,59 @@ get_header();
 ?>
 
 <div class="container">
-        <div id="content-area" class="clearfix">
-            <div id="maw-post-content">
+    <div id="content-area" class="clearfix">
+        <div id="maw-post-content">
 
-                <?php
-                echo '<div class="mw-archive-header">';
-                echo '<center><h1> Archive: Resources</h1></center>';
-                echo '<center><p>Below is an archive of published resources sorted by publish date of the resource.</p></center>';
-                echo '<hr>';
-                echo '<p></p></div>';
+            <?php
+            echo '<div class="mw-archive-header">';
+            echo '<center><h1> Archive: Resources</h1></center>';
+            echo '<center><p>Below is an archive of published resources sorted by publish date of the resource.</p></center>';
+            echo '<hr>';
+            echo '<p></p></div>';
 
-                if(have_posts()) : while(have_posts()) : the_post();
-                    
-                    echo '<article><h2><a href=' . esc_html( get_permalink() ) . '>';
-                    the_title();
-                    echo '</a></h2>';
-
-                    echo '<p class="mw-post-meta-date">';
-                    echo date('F j, Y', get_post_meta( get_the_ID(), 'maw-resource-publish-date', true ) );
-                    echo '</p>';
-
-                    echo '<p></p></article>';
-
-                    endwhile;
-
-                    echo '<hr />';
-
-                    echo '<div class="maw-resources-pagination">';
-                    echo '<div class="row">';
-                    echo '<div class="small-12 columns">';
-                    
-                    the_posts_pagination( array(
-                        'mid_size'  => 2,
-                        'prev_text' => 'Previous',
-                        'next_text' => 'Next',
-                    ) );
-                    
-                    echo '</div>';
-                    echo '</div>';
-                    echo '</div>';
-
-                    else: 
-                    echo '<p>Sorry, no posts matched your criteria.</p>';
-
-                endif;
-
-                wp_reset_postdata(); // Clear the query data for future queries
+            if(have_posts()) : while(have_posts()) : the_post();
                 
-                ?>
+                echo '<article><h2><a href=' . esc_html( get_permalink() ) . '>';
+                the_title();
+                echo '</a></h2>';
+
+                echo '<p class="mw-post-meta-date">';
+                echo date('F j, Y', get_post_meta( get_the_ID(), 'maw-resource-publish-date', true ) );
+                echo '</p>';
+
+                echo '<p></p></article>';
+
+                endwhile;
+
+                echo '<hr />';
+
+                echo '<div class="maw-resources-pagination">';
+                echo '<div class="row">';
+                echo '<div class="small-12 columns">';
+                
+                the_posts_pagination( array(
+                    'mid_size'  => 2,
+                    'prev_text' => 'Previous',
+                    'next_text' => 'Next',
+                ) );
+                
+                echo '</div>';
+                echo '</div>';
+                echo '</div>';
+
+                else: 
+                echo '<p>Sorry, no posts matched your criteria.</p>';
+
+            endif;
+
+            wp_reset_postdata(); // Clear the query data for future queries
+            
+            ?>
 
         </div>
     </div>
 </div>
 
 <?php
+wp_reset_postdata(); // Clear the query data for future queries
 get_footer();
-?>
--- includes/template-overrides/single-maw-resources.php
+++ includes/template-overrides/single-maw-resources.php
@@ -12,41 +12,40 @@ $maw_resource_description_text = get_post_meta( get_the_ID(), 'maw-resource-desc
 $maw_publisher_url = get_post_meta( get_the_ID(), 'maw-resource-url', true );
 ?>
 
-    <div class="container">
-        <div id="content-area" class="clearfix">
-            <div id="maw-post-content">
-                <div id="maw-post-title-meta">
-                    <h2><?php echo get_the_title( $post_id ); ?></h2>
+<div class="container">
+    <div id="content-area" class="clearfix">
+        <div id="maw-post-content">
+            <div id="maw-post-title-meta">
+                <h2><?php echo get_the_title( $post_id ); ?></h2>
 
-                    <p id="maw-post-resource-date-field">
-                        <?php echo date('F j, Y', $maw_publication_publish_date_text ); ?>
-                    </p>
-                </div>
-
-                <div id="maw-post-content-description"> 
-                    <p>
-                        <?php echo esc_html( $maw_resource_description_text ); ?>
-                    </p>
-                </div>
-
-                <div id="maw-post-link">
-                    <p id="maw-post-link-text"><a href=<?php echo esc_html( $maw_publisher_url); ?> target="_blank">View Resource ></p></a>
-                </div>
-
-
-                <?php if($maw_publisher_name_text != "") { ?>
-
-                    <div id="maw-publisher-thank-you">
-                        <p id="maw-publisher-thank-you-text">A special thank you to <?php echo esc_html( $maw_publisher_name_text ); ?> as the original publisher of this resource.</p>
-                    </div>
-                
-                <?php } ?>
-
+                <p id="maw-post-resource-date-field">
+                    <?php echo date('F j, Y', $maw_publication_publish_date_text ); ?>
+                </p>
             </div>
+
+            <div id="maw-post-content-description"> 
+                <p>
+                    <?php echo esc_html( $maw_resource_description_text ); ?>
+                </p>
+            </div>
+
+            <div id="maw-post-link">
+                <p id="maw-post-link-text"><a href=<?php echo esc_html( $maw_publisher_url); ?> target="_blank">View Resource ></p></a>
+            </div>
+
+
+            <?php if($maw_publisher_name_text != "") { ?>
+
+            <div id="maw-publisher-thank-you">
+                <p id="maw-publisher-thank-you-text">A special thank you to <?php echo esc_html( $maw_publisher_name_text ); ?> as the original publisher of this resource.</p>
+            </div>
+            
+            <?php } ?>
+
         </div>
     </div>
+</div>
 
 <?php
 wp_reset_postdata(); // Clear the query data for future queries
-
 get_footer();
